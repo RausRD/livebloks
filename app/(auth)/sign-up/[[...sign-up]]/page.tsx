e584b10f7e8f9cb121221e126e75172fbd039c60
RausRD <dunecruslan2@gmail.com>
2024-07-22 11:21:25 +0300

    authentification with clerk

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+	return (
+		 <main className='autn-page'>
+			 <SignUp/>
+		 </main>
+	);
+};
+
+export default SignUpPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import { cn } from "@/lib/utils"
 import React from "react";
 import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
 	subsets: [ "latin" ],
@@ -15,15 +17,22 @@ export const metadata: Metadata = {
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		 <html lang="en" suppressHydrationWarning>
-		 <body
-				className={cn(
-					 "min-h-screen font-sans antialiased",
-					 fontSans.variable
-				)}
+		 <ClerkProvider
+		  appearance={{
+				baseTheme: dark,
+			  variables: {colorPrimary: '#3371ff', fontSize: '16px'}
+		  }}
 		 >
-		 {children}
-		 </body>
-		 </html>
+			 <html lang="en" suppressHydrationWarning>
+			 <body
+					className={cn(
+						 "min-h-screen font-sans antialiased",
+						 fontSans.variable
+					)}
+			 >
+			 {children}
+			 </body>
+			 </html>
+		 </ClerkProvider>
 	)
 }
